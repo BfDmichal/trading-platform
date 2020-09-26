ae0d5510f4d2d4b6985322e5e999e761c4026294
majkel <michal.przyborowski@o2.pl>
2020-09-26 22:42:48 +0200

    Add photo uploader.
    Added photo uploader with cloudinary properties

--- src/test/java/com/BfDmichal/tradingplatform/domain/CategoryDtoTestSuite.java
+++ src/test/java/com/BfDmichal/tradingplatform/domain/CategoryDtoTestSuite.java
@@ -3,7 +3,6 @@ package com.BfDmichal.tradingplatform.domain;
 import com.BfDmichal.tradingplatform.repositories.CategoryRepository;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
