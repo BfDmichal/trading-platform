a41ec59ad4715d3934fbecf2c2fa738cd14b3070
majkel <michal.przyborowski@o2.pl>
2020-10-09 08:30:46 +0200

    test fix

--- src/test/java/com/BfDmichal/tradingplatform/TradingPlatformApplicationTests.java
+++ src/test/java/com/BfDmichal/tradingplatform/TradingPlatformApplicationTests.java
@@ -1,13 +1,16 @@
-//package com.BfDmichal.tradingplatform;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class TradingPlatformApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.BfDmichal.tradingplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
